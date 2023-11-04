59a04ed66c7135469b4cb3f72b9c828e409979be
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2023-11-04 05:32:24 +0200

    Add project files.

---
+++ Calculate/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Calculate
+{
+    public int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Calculate calculator = new Calculate();
+
+        int result1 = calculator.Addition(5, 3);
+        Console.WriteLine("Addition result: " + result1);
+
+        int result2 = calculator.Subtraction(10, 4);
+        Console.WriteLine("Subtraction result: " + result2);
+    }
+}
+
